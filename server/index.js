78dec421971debc387ef378ec97667ba7bad8292
Nathan <nathanrodriguez240@gmail.com>
2023-11-30 14:24:57 -0500

    basic server setup

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {console.log("listening from port 5000")});
+app.listen(5000, () => {
+	console.log("listening from port 5000");
+})
 
-app.post("/", async (req, res) => {
-	res.json("Hello World");
+app.get("/", async (req, res) => {
+	res.send("Hello World");
 })
