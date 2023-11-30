0478a2826bc1beeae6ed505e0e835026b05506c7
Nathan <nathanrodriguez240@gmail.com>
2023-11-30 14:53:10 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const cors = require('cors');
 app.use(cors());
