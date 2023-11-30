d903a08892e77953e0998935744ac337ae82b0b1
Nathan <nathanrodriguez240@gmail.com>
2023-11-30 14:20:58 -0500

    small updates

--- server/index.js
+++ server/index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+app.use(cors);
+app.use(express.json());
+
+app.listen(5000, () => {console.log("listening from port 5000")});
+
+app.post("/", async (req, res) => {
+	res.json("Hello World");
+})
