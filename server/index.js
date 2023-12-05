2fe551473e5839038b070fc9054f86442c3c3282
Nathan <nathanrodriguez240@gmail.com>
2023-12-04 20:28:29 -0500

    got login kind of working

--- server/index.js
+++ server/index.js
@@ -21,10 +21,28 @@ app.get("/", async (req, res) => {
 })
 
 // creates a new user
-app.post("/users", async (req, res) => {
+app.post("/signup", async (req, res) => {
     try{
         const user = await User.create(req.body)
         res.status(200).json(user);
+    }catch(e){
+        console.error(e);
+    }
+})
+
+
+
+app.get("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+
+        if(!user) {
+            res.status(401).json({msg: "Wrong email!"});
+        } else if(user.password != req.body.password) {
+            res.status(402).json({msg: "Wrong password!"});
+        } else {
+            res.status(200).json(user);
+        }
     }catch(e){
         console.error(e);
     }
